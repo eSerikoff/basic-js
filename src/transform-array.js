1d4c2b554671e35ce36a035c7cc54cad2884fea2
Yauhen Serykau <mail.serikoff@gmail.com>
2023-04-14 20:43:26 +0300

    solved some problems

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,53 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
+function transform(/*  arr  */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+/*
+  if (!Array.isArray(arr)) {
+    console.log("'arr' parameter must be an instance of the Array!");
+    return false;
+  } else {
+
+    let tempArr = [];
+
+    for (let i = 0; i < arr.length; i++){
+      if (typeof arr[i] !== 'string'){
+        tempArr.push(arr[i]);
+      } else {
+        switch (arr[i]) {
+          case '--discard-next':
+            i++;
+            break;
+          case '--discard-prev':
+            tempArr.pop();
+            break;
+          case '--double-next':
+            tempArr.push(arr[i + 1]);
+            break;
+          case '--double-prev':
+            tempArr.push(arr[i - 1]);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    return tempArr;
+  }
+*/
+
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -2,18 +2,59 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
+function getSeason(/*  date  */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  /*
+  if (date === null){
+    console.log('Unable to determine the time of year!');
+  }
+  if (typeof date !== 'object'){
+    console.log('Invalid date!');
+  }
+
+  let String = '';
+
+  switch (date.getMonth()) {
+    case 11:
+    case 0:
+    case 1:
+      String = 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      String = 'spring';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      String = 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      String = 'summer';
+      break;
+    case 8:
+    case 9:
+    case 10:
+      String = 'fall';
+      break;
+    default:
+      break;
+  }
+  return String;
+  */
 }
 
 module.exports = {
